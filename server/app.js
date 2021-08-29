1aa901ca8669f25341ace73f1d621d99749f0bbe
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-29 20:14:40 +0300

    create server and serve public Relates #13

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { join } = require('path');
+const compression = require('compression');
+
+const app = express();
+app.use(compression());
+app.disabled('x-powered-by');
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(join(__dirname, '..', 'public'), { maxAge: '30d' }));
+app.set('port', process.env.PORT || 3000);
+
+module.exports = app;
